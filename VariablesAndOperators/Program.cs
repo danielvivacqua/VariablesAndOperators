c0ce9dc2d887cb1a077e74dcbba053ec67de46ee
danielvivacqua <drvivacqua@hotmail.com>
2017-06-27 12:18:45 -0400

    More practice

--- VariablesAndOperators/Program.cs
+++ VariablesAndOperators/Program.cs
@@ -10,6 +10,43 @@ namespace VariablesAndOperators
     {
         static void Main(string[] args)
         {
+            int age = 65;
+            string why = "because I said so";
+            char firstInitial = 'L';
+
+            //floating point types
+            float floatNumber = 2.164403304897294849383961032f;
+            Console.WriteLine(floatNumber);
+
+            double doubleNumber = 2.164403304897294849383961032d;
+            Console.WriteLine(doubleNumber);
+
+            decimal decimalNumber = 2.164403304897294849383961032m;
+            Console.WriteLine(decimalNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ////initializing new firstname variable
+            //string firstName = "Mike";
+            ////Printing the value of firstName to the console
+            //Console.WriteLine(firstName);
+
+            //int birthMonth = 10;
+            //Console.WriteLine(birthMonth);
+
+
+
         }
     }
 }
