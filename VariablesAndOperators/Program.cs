fa9953a1dba4af54a818d5f4596af201b3f903a9
danielvivacqua <drvivacqua@hotmail.com>
2017-06-28 10:46:07 -0400

    Added t-shirt comments

--- VariablesAndOperators/Program.cs
+++ VariablesAndOperators/Program.cs
@@ -10,33 +10,54 @@ namespace VariablesAndOperators
     {
         static void Main(string[] args)
         {
+            //Intro statement
+            //Tell user how much each t-shirt costs
+            //Ask how many Super Swim t-shirts the user wants to purchase
+            //Get user input
+            //Ask how many Be The Shark t-shirts the user wants to purchase
+            //Get user input
+            //Ask how many Baby Bubbles t-shirts the user wants to purchase
+            //Get user input
+
+            //Define the values of each t-shirt type
+
+            //MATH
+            //Get total number of shirts sold using addition
+            //Get the subtotal using multiplication and addition
+            //Get the grand total by multiplying the subtotal by tax amount (8% or .08)
+            //and adding this tax amount to the original subtotal (or multiplying by 1.08)
+
+            //Print the receipt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             //    int age = 65;
             //    string why = "because I said so";
             //    char firstInitial = 'L';
 
-            Console.WriteLine("What is the name on the reservation?");
-            string reserveName = Console.ReadLine();
+            //Console.WriteLine("What is the name on the reservation?");
+            //string reserveName = Console.ReadLine();
 
-            Console.WriteLine("How many people are in your party?");
-            int numberOfPeople = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How many people are in your party?");
+            //int numberOfPeople = int.Parse(Console.ReadLine());
 
-            double amountEach = 13.53d;
+            //double amountEach = 13.53d;
 
-            double totalBill = numberOfPeople * amountEach;
-            Console.WriteLine("Reservation: " +  reserveName);
-            Console.WriteLine("Total Due: $" + totalBill);
-
-
-
-
-
-
-
-
-
-
-
-
+            //double totalBill = numberOfPeople * amountEach;
+            //Console.WriteLine("Reservation: " +  reserveName);
+            //Console.WriteLine("Total Due: $" + totalBill);
 
 
             ////initializing new firstname variable
